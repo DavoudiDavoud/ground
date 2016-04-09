2f7353fb610e91953d99605f266cdb714e94c089
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-09 13:28:10 +0100

    Update window.h

--- window.h
+++ window.h
@@ -1,8 +1,8 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
-#include <qwt/qwt_thermo.h>
-#include <qwt/qwt_knob.h>
+
+
 #include <qwt/qwt_plot.h>
 #include <qwt/qwt_plot_curve.h>
 
@@ -28,23 +28,21 @@ public slots:
 
 // internal variables for the window class
 private:
-	QwtKnob      *knob;
-	QwtThermo    *thermo;
+	
+
 	QwtPlot      *plot;
 	QwtPlotCurve *curve;
 
 	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
-	QVBoxLayout  *vLayout;  // vertical layout
+	
 	QHBoxLayout  *hLayout;  // horizontal layout
 
-	static const int plotDataSize = 100;
+	static const int plotDataSize = 4000;
 
 	// data arrays for the plot
 	double xData[plotDataSize];
 	double yData[plotDataSize];
 
-	double gain;
-	int count;
 
 //	ADCreader *adcreader;
 };
