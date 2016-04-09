220b99d1b738f2e5b1790cfef74a800df1b650b3
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-09 14:02:31 +0100

    Update adcreader.h

--- adcreader.h
+++ adcreader.h
@@ -2,15 +2,25 @@
 #define ADCREADER
 
 #include <QThread>
-
+ 
 class ADCreader : public QThread
 {
 public:
-	ADCreader() {running = 0;};
+	ADCreader();
 	void quit();
 	void run();
-private:
-	bool running;
+	int get_samples();
+	bool read_enable();
+	
+	int ret;
+	int fd;
+	int sysfs_fd;
+	bool running; 
+	int no_tty;
+	int inp;
+	int outp;
+	int buff[100];
+
 
 };
 
