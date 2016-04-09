7addc5678d63aa69ed4784ea0832a8f757c09bf5
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-09 13:31:33 +0100

    Update window.cpp

--- window.cpp
+++ window.cpp
@@ -1,24 +1,13 @@
 #include "window.h"
-// #include "adcreader.h"
+#include "adcreader.h"
 
 #include <cmath>  // for sine stuff
 
 
-Window::Window() : gain(5), count(0)
+Window::Window()
 {
-	knob = new QwtKnob;
-	// set up the gain knob
-	knob->setValue(gain);
+	
 
-	// use the Qt signals/slots framework to update the gain -
-	// every time the knob is moved, the setGain function will be called
-	connect( knob, SIGNAL(valueChanged(double)), SLOT(setGain(double)) );
-
-	// set up the thermometer
-	thermo = new QwtThermo; 
-	thermo->setFillBrush( QBrush(Qt::red) );
-	thermo->setRange(0, 20);
-	thermo->show();
 
 
 	// set up the initial plot data
@@ -38,16 +27,9 @@ Window::Window() : gain(5), count(0)
 	plot->show();
 
 
-	// set up the layout - knob above thermometer
-	vLayout = new QVBoxLayout;
-	vLayout->addWidget(knob);
-	vLayout->addWidget(thermo);
-
 	// plot to the left of knob and thermometer
 	hLayout = new QHBoxLayout;
-	hLayout->addLayout(vLayout);
 	hLayout->addWidget(plot);
-
 	setLayout(hLayout);
 
 	// This is a demo for a thread which can be
@@ -55,21 +37,21 @@ Window::Window() : gain(5), count(0)
 	// At the moment it doesn't do anything else than
 	// running in an endless loop and which prints out "tick"
 	// every second.
-//	adcreader = new ADCreader();
-//	adcreader->start();
+	adcreader = new ADCreader();
+	adcreader->start();
 }
 
 Window::~Window() {
 	// tells the thread to no longer run its endless loop
-//	adcreader->quit();
+	adcreader->quit();
 	// wait until the run method has terminated
-//	adcreader->wait();
-//	delete adcreader;
+	adcreader->wait();
+	delete adcreader;
 }
 
 void Window::timerEvent( QTimerEvent * )
 {
-	double inVal = gain * sin( M_PI * count/50.0 );
+	double inVal =sin( M_PI * count/50.0 );
 	++count;
 
 	// add the new input to the plot
@@ -78,14 +60,7 @@ void Window::timerEvent( QTimerEvent * )
 	curve->setSamples(xData, yData, plotDataSize);
 	plot->replot();
 
-	// set the thermometer value
-	thermo->setValue( inVal + 10 );
+	
 }
 
 
-// this function can be used to change the gain of the A/D internal amplifier
-void Window::setGain(double gain)
-{
-	// for example purposes just change the amplitude of the generated input
-	this->gain = gain;
-}
